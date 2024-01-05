7e2331698d9f77a609591dde54350782f2975374
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-01-05 22:06:12 +0000

    Deploying to gh-pages from @ MrMatvienko/goit-react-hw-08-phonebook@6bc587e18696a0e61c287655232a2e6d9fb8cca6 🚀

---
+++ static/js/266.01d53b80.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184);function o(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h1",{children:"Welcome to Phonebook!"})," "]})}}}]);
+//# sourceMappingURL=266.01d53b80.chunk.js.map
