d408f1d85e5e27b70a3d827828bc428e8e60a24d
MrMatvienko <133213584+MrMatvienko@users.noreply.github.com>
2024-01-06 19:52:47 +0200

    comit4

--- src/store/auth/operations.js
+++ src/store/auth/operations.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
-
-// axios.defaults.baseURL = 'https://phonebook-db-2lk4.onrender.com/';
-
 const setAuthHeader = token => {
   // Встановлення заголовка авторизації у вихідних параметрах запиту
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
